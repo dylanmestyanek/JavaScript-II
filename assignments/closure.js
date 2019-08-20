7b15f37fde0c3845ce79527bfaf128342ada2e13
Dylan Mestyanek <mestyanekdylan@gmail.com>
2019-08-20 10:56:59 -0500

    mvp complete

--- assignments/closure.js
+++ assignments/closure.js
@@ -5,29 +5,58 @@
 // The outer scope can be a parent function, or the top level of the script.
 
 
+let total = 0;
+function yeehaw(){
+ for (let i = 1; i < 10; i++){
+  total += i;
+ }
+ return total;
+}
+yeehaw();
+
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 
 // ==== Challenge 2: Implement a "counter maker" function ====
 const counterMaker = () => {
-  // IMPLEMENTATION OF counterMaker:
-  // 1- Declare a `count` variable with a value of 0. We will be mutating it, so declare it using `let`!
-  // 2- Declare a function `counter`. It should increment and return `count`.
-  //      NOTE: This `counter` function, being nested inside `counterMaker`,
-  //      "closes over" the `count` variable. It can "see" it in the parent scope!
-  // 3- Return the `counter` function.
+  let count = 1;
+
+  return function counter(){
+    return count++;
+  }
 };
-// Example usage: const myCounter = counterMaker();
-// myCounter(); // 1
-// myCounter(); // 2
+
+const newCounter = counterMaker();
 
 // ==== Challenge 3: Make `counterMaker` more sophisticated ====
 // It should have a `limit` parameter. Any counters we make with `counterMaker`
 // will refuse to go over the limit, and start back at 1.
+const counterMaker = (limit) => {
+  let count = 1;
+
+  return function counter(){
+    return count <= limit ? count++ : count = 1;
+  }
+};
+
+const newCounter = counterMaker(3);
 
 // ==== Challenge 4: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
-  // Return an object that has two methods called `increment` and `decrement`.
-  // `increment` should increment a counter variable in closure scope and return it.
-  // `decrement` should decrement the counter variable and return it.
+  let count = 1;
+
+  return function(){
+    return {
+      increment: function(){ return count++; },
+      decrement: function(){ return count--; }
+    }
+  }
 };
+
+const newCounter = counterFactory();
+
+
+
+
+
+
